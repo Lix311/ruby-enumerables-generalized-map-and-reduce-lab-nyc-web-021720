0c174462f349b254c2fec6dba4627d5797fba318
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-11 00:49:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  index = 0 
+  while index < array.length 
+    base = array[index]
+    new_array.push(yield(base))
+  index += 1 
+  end 
+return new_array    
+end 
+
+def reduce(array, sv = nil)
+  if sv
+    sum = sv 
+  else 
+    sum = array[0]
+  end 
+    
+  index = 0
+  while index < array.length
+    sum = yield(sum,array[index])
+    index += 1 
+  end 
+ sum    
+end 
